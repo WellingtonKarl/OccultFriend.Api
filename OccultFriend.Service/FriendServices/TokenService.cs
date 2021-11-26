c35f40a24f5068ea64f485b153658acf1f2601ab
Wellington Karl <wellington.karl@imusica.com.br>
2021-11-26 17:21:59 -0300

    Resolve bug de identity name vir null

--- OccultFriend.Service/FriendServices/TokenService.cs
+++ OccultFriend.Service/FriendServices/TokenService.cs
@@ -2,6 +2,7 @@
 using OccultFriend.Domain.Model;
 using OccultFriend.Service.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -17,21 +18,28 @@ namespace OccultFriend.Service.FriendServices
             _secret = secret;
         }
 
-        public string GenerateToken(Friend friend)
+        public string GenerateToken(Friend friend, ClaimsPrincipal user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_secret);
+            var identity = new ClaimsIdentity
+                (
+                    new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, friend.Name),
+                        new Claim(ClaimTypes.Email, friend.Email)
+                    },
+                    authenticationType: "Password"
+                );
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, friend.Name.ToString()),
-                    new Claim(ClaimTypes.Email, friend.Email.ToString())
-                }),
+                Subject = identity,
                 Expires = DateTime.UtcNow.AddMinutes(30),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
+
+            user.AddIdentity(identity);
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
